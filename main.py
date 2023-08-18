cf1b2031e20259f637d59e234b2662b8727ef32c
банан <beloys0071@gmail.com>
2023-08-18 10:04:33 +0300

    кто прочитал тот лох

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 from aiogram.utils import executor
 from aiogram import Bot, types, Dispatcher
-
+from dotenv import load_dotenv
+load_dotenv()
 
 bot = Bot(token=os.environ.get('TOKEN'), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -10,12 +11,20 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.answer('Hello some')
-
-
+    await message.answer('''
+Здравствуйте, этот бот создан
+для того чтобы помочь найти вам команду.
+Надеемся что у вас это получится
+и опасайтесь мошенников.''')
 def main():
     executor.start_polling(dp)
 
-
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
