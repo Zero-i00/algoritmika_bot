96a665d31d623221e5d5858d860cc260f1bd3974
HP <borovikovanton@gmail.com>
2023-08-18 12:13:34 +0500

    add bot keyboard

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import os
 
+from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
 from aiogram import Bot, types, Dispatcher
+from dotenv import load_dotenv
 
+load_dotenv()
 
 bot = Bot(token=os.environ.get('TOKEN'), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands='start')
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer('Hello some')
+    start_buttons = ['Помощь', 'Найти команду']
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.add(*start_buttons)
+    await message.answer('Ку огузок', reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals='Помощь'))
+async def get_phones(message: types.Message):
+    await message.answer('''Помощь: 
+    Что может этот бот?
+    Он может помоч вам нати команду.
+    Как её найти?
+    Чтобы её найти надо нажать на кнопку  найти команду.''')
+
+
+@dp.message_handler(Text(equals='Найти команду'))
+async def get_phones(message: types.Message):
+    await message.answer('''Когда бот найдет подходящюю команду для вас он вам напишет
+''')
+
 
 
 def main():
